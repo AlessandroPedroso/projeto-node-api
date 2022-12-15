8b828dfa2b769a89eff6cf7da55d8c7767c1cbd0
AlessandroPedroso <alessandropedrosoti@gmail.com>
2022-12-15 19:34:16 -0300

    primeiro commit do projeto

---
+++ index.js
@@ -0,0 +1,22 @@
+import { request, response } from "express";
+import express from 'express';
+
+const app = express()
+
+const users = []
+
+app.get('/users', (request,response) =>{
+
+    return response.json(users)
+})
+
+app.post('/users', (request,response) =>{
+
+    console.log(request)
+
+    return response.json({ok:true})
+})
+
+
+
+app.listen(3000)
